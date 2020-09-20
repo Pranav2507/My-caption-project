10d8c5a624ae5e1513b411f000be1090755c2330
Pranav2507 <71224908+Pranav2507@users.noreply.github.com>
2020-09-20 17:07:06 +0530

    Add files via upload

---
+++ FIbonacci.py
@@ -0,0 +1,25 @@
+num=int(input("How many terms? "))
+n1, n2 = 0, 1
+count = 0
+num== 1
+print("Fibonacci sequence:")
+while count < num:
+    print(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
